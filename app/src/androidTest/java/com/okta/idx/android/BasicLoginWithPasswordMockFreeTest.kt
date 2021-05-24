7040f8ae9ad830b0daa0f7651015d731e08a4528
Fei Chen <fei.chen@okta.com>
2021-05-24 10:45:20 +0300

    scenario 1.1.8

---
+++ app/src/androidTest/java/com/okta/idx/android/BasicLoginWithPasswordMockFreeTest.kt
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2021-Present Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.idx.android
+
+import androidx.lifecycle.Lifecycle
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.okta.idx.android.infrastructure.espresso.waitForElement
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class BasicLoginWithPasswordMockFreeTest {
+
+    companion object {
+        private const val FORGOT_PASSWORD_BUTTON = "com.okta.idx.android:id/forgot_password_button"
+    }
+
+    @get:Rule val activityRule = ActivityScenarioRule(MainActivity::class.java)
+
+    @Test fun scenario_1_1_8_Mary_clicks_on_the_forgot_password_link() {
+        activityRule.scenario.moveToState(Lifecycle.State.RESUMED)
+        onView(withId(R.id.login_button)).perform(click())
+
+        waitForElement(FORGOT_PASSWORD_BUTTON)
+        onView(withId(R.id.forgot_password_button)).perform(click())
+
+        onView(withId(R.id.username_edit_text)).check(matches(isDisplayed()))
+        onView(withId(R.id.forgot_password_button)).check(matches(isDisplayed()))
+    }
+}
