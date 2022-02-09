604cd2646c010e649ef18f43e3a17beea1f100d4
Jay Newstrom <jaynewstrom@gmail.com>
2022-02-09 15:03:58 -0600

    OKTA-464627 Add docs for TokenStorage.

--- auth-foundation/src/main/java/com/okta/authfoundation/credential/Token.kt
+++ auth-foundation/src/main/java/com/okta/authfoundation/credential/Token.kt
@@ -18,13 +18,37 @@ package com.okta.authfoundation.credential
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
+/**
+ * Token information representing a user's access to a resource server, including access token, refresh token, and other related information.
+ */
 @Serializable
-data class Token(
+data class Token internal constructor(
+    /**
+     * The string type of the token (e.g. `Bearer`).
+     */
     @SerialName("token_type") val tokenType: String,
+    /**
+     * The expiration duration in seconds for this token.
+     */
     @SerialName("expires_in") val expiresIn: Int,
+    /**
+     * The access token.
+     */
     @SerialName("access_token") val accessToken: String,
+    /**
+     * The scopes granted when this token was minted.
+     */
     @SerialName("scope") val scope: String,
+    /**
+     * The refresh token, if requested.
+     */
     @SerialName("refresh_token") val refreshToken: String? = null,
+    /**
+     * The ID token, if requested.
+     */
     @SerialName("id_token") val idToken: String? = null,
+    /**
+     * The device secret, if requested.
+     */
     @SerialName("device_secret") val deviceSecret: String? = null,
 )
--- auth-foundation/src/main/java/com/okta/authfoundation/credential/TokenStorage.kt
+++ auth-foundation/src/main/java/com/okta/authfoundation/credential/TokenStorage.kt
@@ -15,11 +15,56 @@
  */
 package com.okta.authfoundation.credential
 
+import com.okta.authfoundation.OktaSdkDefaults
+
+/**
+ * Interface used to customize the way tokens are stored, updated, and removed throughout the lifecycle of an application.
+ *
+ * A default implementation is provided, but for advanced use-cases, you may implement this protocol yourself and assign an instance to the [OktaSdkDefaults.storage] property.
+ *
+ * Warning: When implementing a custom [TokenStorage] class, it's vitally important that you do not directly invoke any of these methods yourself. These methods are intended to be called on-demand by the other AuthFoundation classes, and the behavior is undefined if these methods are called directly by the developer.
+ */
 interface TokenStorage {
+    /**
+     * Used to access all [Entry]s in storage.
+     *
+     *  @return all [Entry] in storage.
+     */
     suspend fun entries(): List<Entry>
+
+    /**
+     *  Add a new [Entry] to storage.
+     *
+     *  @param entry the [Entry] to add.
+     */
     suspend fun add(entry: Entry)
+
+    /**
+     *  Remove an existing [Entry] from storage.
+     *
+     *  @param entry the [Entry] to remove.
+     */
     suspend fun remove(entry: Entry)
+
+    /**
+     *  Replace an existing [Entry] in storage with a new [Entry].
+     *
+     *  @param existingEntry the existing [Entry] in storage.
+     *  @param updatedEntry the new [Entry] to store.
+     */
     suspend fun replace(existingEntry: Entry, updatedEntry: Entry)
 
-    data class Entry(val token: Token, val metadata: Map<String, String>)
+    /**
+     *  Represents the data to store in [TokenStorage].
+     */
+    data class Entry(
+        /**
+         *  The [Token] associated with the [TokenStorage] entry.
+         */
+        val token: Token,
+        /**
+         *  The metadata associated with the [TokenStorage] entry.
+         */
+        val metadata: Map<String, String>,
+    )
 }
