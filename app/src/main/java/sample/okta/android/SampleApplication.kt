37913cb2ccf22472ba8e9cb12dfa1a2074a17c64
Jay Newstrom <jaynewstrom@gmail.com>
2022-04-12 13:02:58 -0500

    HTTP Headers Clock Example.

--- app/src/main/java/sample/okta/android/SampleApplication.kt
+++ app/src/main/java/sample/okta/android/SampleApplication.kt
@@ -18,12 +18,22 @@ package sample.okta.android
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
+import com.okta.authfoundation.AuthFoundationDefaults
 import com.okta.authfoundation.client.OidcClient
+import com.okta.authfoundation.client.OidcClock
 import com.okta.authfoundation.client.OidcConfiguration
 import com.okta.authfoundation.credential.CredentialDataSource.Companion.createCredentialDataSource
 import com.okta.authfoundationbootstrap.CredentialBootstrap
+import okhttp3.Cache
 import okhttp3.HttpUrl.Companion.toHttpUrl
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
+import okhttp3.Response
 import timber.log.Timber
+import java.io.File
+import java.text.SimpleDateFormat
+import java.time.Instant
+import java.util.Locale
 
 class SampleApplication : Application() {
     companion object {
@@ -38,6 +48,21 @@ class SampleApplication : Application() {
 
         Timber.plant(Timber.DebugTree())
 
+        val clock = NetworkHeadersClock()
+        AuthFoundationDefaults.clock = clock
+
+        AuthFoundationDefaults.okHttpClientFactory = {
+            val httpCacheDirectory = File(applicationContext.cacheDir, "http-cache")
+            val cacheSize = 100 * 1024 * 1024 // 100 MiB
+            val cache = Cache(httpCacheDirectory, cacheSize.toLong())
+
+            OkHttpClient.Builder()
+                .cache(cache)
+                .hostnameVerifier { _, _ -> true }
+                .addInterceptor(clock)
+                .build()
+        }
+
         val oidcConfiguration = OidcConfiguration(
             clientId = BuildConfig.CLIENT_ID,
             defaultScopes = setOf("openid", "email", "profile", "offline_access"),
@@ -49,5 +74,33 @@ class SampleApplication : Application() {
             "${BuildConfig.ISSUER}/.well-known/openid-configuration".toHttpUrl(),
         )
         CredentialBootstrap.initialize(oidcClient.createCredentialDataSource(this))
+    }
+}
+
+class NetworkHeadersClock : OidcClock, Interceptor {
+    private val dateFormat = object : ThreadLocal<SimpleDateFormat>() {
+        override fun initialValue(): SimpleDateFormat {
+            // Tue, 12 Apr 2022 17:29:43 GMT
+            return SimpleDateFormat("EEE, d MMM yyyy HH:mm:ss z", Locale.US)
+        }
+    }
+
+    @Volatile private var offset: Long = 0
+
+    override suspend fun currentTimeEpochSecond(): Long {
+        return Instant.now().epochSecond + offset
+    }
+
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+        if (response.networkResponse != null) { // We're not looking at a cached header.
+            response.headers("date").firstOrNull()?.let { dateHeaderValue ->
+                val date = dateFormat.get()?.parse(dateHeaderValue)
+                if (date != null) {
+                    offset = date.toInstant().epochSecond - Instant.now().epochSecond
+                }
+            }
+        }
+        return response
     }
 }
