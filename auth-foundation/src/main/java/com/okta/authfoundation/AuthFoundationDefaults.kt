a732055f3486dcfce8099186f4c13c4a9a58824e
Rajdeep Nanua <rajdeep.nanua@okta.com>
2025-01-30 14:51:11 -0500

    Make AuthFoundationDefaults configurable by other SDKs

--- auth-foundation/src/main/java/com/okta/authfoundation/AuthFoundationDefaults.kt
+++ auth-foundation/src/main/java/com/okta/authfoundation/AuthFoundationDefaults.kt
@@ -15,29 +15,32 @@
  */
 package com.okta.authfoundation
 
+import com.okta.authfoundation.SdkDefaults.getAccessTokenValidator
+import com.okta.authfoundation.SdkDefaults.getCacheFactory
+import com.okta.authfoundation.SdkDefaults.getClock
+import com.okta.authfoundation.SdkDefaults.getComputeDispatcher
+import com.okta.authfoundation.SdkDefaults.getCookieJar
+import com.okta.authfoundation.SdkDefaults.getDeviceSecretValidator
+import com.okta.authfoundation.SdkDefaults.getEventCoordinator
+import com.okta.authfoundation.SdkDefaults.getIdTokenValidator
+import com.okta.authfoundation.SdkDefaults.getIoDispatcher
+import com.okta.authfoundation.SdkDefaults.getLoginCancellationDebounceTime
+import com.okta.authfoundation.SdkDefaults.getOkHttpClientFactory
+import com.okta.authfoundation.SdkDefaults.getTokenEncryptionHandler
+import com.okta.authfoundation.SdkDefaults.getTokenStorageFactory
 import com.okta.authfoundation.client.AccessTokenValidator
 import com.okta.authfoundation.client.Cache
-import com.okta.authfoundation.client.DefaultAccessTokenValidator
-import com.okta.authfoundation.client.DefaultDeviceSecretValidator
-import com.okta.authfoundation.client.DefaultIdTokenValidator
 import com.okta.authfoundation.client.DeviceSecretValidator
 import com.okta.authfoundation.client.IdTokenValidator
 import com.okta.authfoundation.client.OidcClock
-import com.okta.authfoundation.client.SharedPreferencesCache
-import com.okta.authfoundation.credential.DefaultTokenEncryptionHandler
-import com.okta.authfoundation.credential.RoomTokenStorage
 import com.okta.authfoundation.credential.Token
 import com.okta.authfoundation.credential.TokenEncryptionHandler
 import com.okta.authfoundation.credential.TokenStorage
 import com.okta.authfoundation.events.EventCoordinator
-import kotlinx.coroutines.Dispatchers
 import okhttp3.Call
 import okhttp3.CookieJar
-import okhttp3.OkHttpClient
-import java.time.Instant
 import kotlin.coroutines.CoroutineContext
 import kotlin.time.Duration
-import kotlin.time.Duration.Companion.seconds
 
 /**
  *  The defaults used in various classes throughout the rest of the SDK.
@@ -48,45 +51,45 @@ import kotlin.time.Duration.Companion.seconds
  */
 object AuthFoundationDefaults {
     /** The default Call.Factory. */
-    var okHttpClientFactory: () -> Call.Factory by NoSetAfterGetWithLazyDefaultFactory { { OkHttpClient() } }
+    var okHttpClientFactory: () -> Call.Factory by NoSetAfterGetWithLazyDefaultFactory { getOkHttpClientFactory() }
 
     /** The CoroutineDispatcher which should be used for IO bound tasks. */
-    var ioDispatcher: CoroutineContext by NoSetAfterGetWithLazyDefaultFactory { Dispatchers.IO }
+    var ioDispatcher: CoroutineContext by NoSetAfterGetWithLazyDefaultFactory { getIoDispatcher() }
 
     /** The CoroutineDispatcher which should be used for compute bound tasks. */
-    var computeDispatcher: CoroutineContext by NoSetAfterGetWithLazyDefaultFactory { Dispatchers.Default }
+    var computeDispatcher: CoroutineContext by NoSetAfterGetWithLazyDefaultFactory { getComputeDispatcher() }
 
     /** The default EventCoordinator. */
-    var eventCoordinator: EventCoordinator by NoSetAfterGetWithLazyDefaultFactory { EventCoordinator(emptyList()) }
+    var eventCoordinator: EventCoordinator by NoSetAfterGetWithLazyDefaultFactory { getEventCoordinator() }
 
     /** The default OidcClock. */
-    var clock: OidcClock by NoSetAfterGetWithLazyDefaultFactory { OidcClock { Instant.now().epochSecond } }
+    var clock: OidcClock by NoSetAfterGetWithLazyDefaultFactory { getClock() }
 
     /** The default IdTokenValidator. */
-    var idTokenValidator: IdTokenValidator by NoSetAfterGetWithLazyDefaultFactory { DefaultIdTokenValidator() }
+    var idTokenValidator: IdTokenValidator by NoSetAfterGetWithLazyDefaultFactory { getIdTokenValidator() }
 
     /** The default AccessTokenValidator. */
-    var accessTokenValidator: AccessTokenValidator by NoSetAfterGetWithLazyDefaultFactory { DefaultAccessTokenValidator() }
+    var accessTokenValidator: AccessTokenValidator by NoSetAfterGetWithLazyDefaultFactory { getAccessTokenValidator() }
 
     /** The default DeviceSecretValidator. */
-    var deviceSecretValidator: DeviceSecretValidator by NoSetAfterGetWithLazyDefaultFactory { DefaultDeviceSecretValidator() }
+    var deviceSecretValidator: DeviceSecretValidator by NoSetAfterGetWithLazyDefaultFactory { getDeviceSecretValidator() }
 
     /** The default function that returns a singleton instance of [Cache]. */
-    var cacheFactory: suspend () -> Cache by NoSetAfterGetWithLazyDefaultFactory { { SharedPreferencesCache.getInstance() } }
+    var cacheFactory: suspend () -> Cache by NoSetAfterGetWithLazyDefaultFactory { getCacheFactory() }
 
     /** The default function that retuns a singleton instance of [TokenStorage]. */
-    var tokenStorageFactory: suspend () -> TokenStorage by NoSetAfterGetWithLazyDefaultFactory { { RoomTokenStorage.getInstance() } }
+    var tokenStorageFactory: suspend () -> TokenStorage by NoSetAfterGetWithLazyDefaultFactory { getTokenStorageFactory() }
 
     /** The default [TokenEncryptionHandler] for encrypting and decrypting stored [Token]s.*/
-    var tokenEncryptionHandler: TokenEncryptionHandler by NoSetAfterGetWithLazyDefaultFactory { DefaultTokenEncryptionHandler() }
+    var tokenEncryptionHandler: TokenEncryptionHandler by NoSetAfterGetWithLazyDefaultFactory { getTokenEncryptionHandler() }
 
     /** The default [CookieJar]. By default, this is [CookieJar.NO_COOKIES]. */
-    var cookieJar: CookieJar by NoSetAfterGetWithLazyDefaultFactory { CookieJar.NO_COOKIES }
+    var cookieJar: CookieJar by NoSetAfterGetWithLazyDefaultFactory { getCookieJar() }
 
     /** The default wait time until the web login flow is cancelled after receiving empty redirect response from the web browser.
      * This can resolve some issues caused by older devices when invalid redirect results are returned from the older browser. When this is set to a non-zero value, it introduces a
      * delay to all redirects when an error is received. */
-    var loginCancellationDebounceTime: Duration by NoSetAfterGetWithLazyDefaultFactory { 0.seconds }
+    var loginCancellationDebounceTime: Duration by NoSetAfterGetWithLazyDefaultFactory { getLoginCancellationDebounceTime() }
 
     object Encryption {
         /** The default keyAlias for the encryption key that will be used for encrypting the stored Token objects */
---
+++ auth-foundation/src/main/java/com/okta/authfoundation/SdkDefaults.kt
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2025-Present Okta, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.okta.authfoundation
+
+import com.okta.authfoundation.client.AccessTokenValidator
+import com.okta.authfoundation.client.Cache
+import com.okta.authfoundation.client.DefaultAccessTokenValidator
+import com.okta.authfoundation.client.DefaultDeviceSecretValidator
+import com.okta.authfoundation.client.DefaultIdTokenValidator
+import com.okta.authfoundation.client.DeviceSecretValidator
+import com.okta.authfoundation.client.IdTokenValidator
+import com.okta.authfoundation.client.OidcClock
+import com.okta.authfoundation.client.SharedPreferencesCache
+import com.okta.authfoundation.credential.DefaultTokenEncryptionHandler
+import com.okta.authfoundation.credential.RoomTokenStorage
+import com.okta.authfoundation.credential.TokenEncryptionHandler
+import com.okta.authfoundation.credential.TokenStorage
+import com.okta.authfoundation.events.EventCoordinator
+import kotlinx.coroutines.Dispatchers
+import okhttp3.Call
+import okhttp3.CookieJar
+import okhttp3.OkHttpClient
+import java.time.Instant
+import kotlin.coroutines.CoroutineContext
+import kotlin.time.Duration
+import kotlin.time.Duration.Companion.seconds
+
+@InternalAuthFoundationApi
+object SdkDefaults {
+    var getOkHttpClientFactory: () -> (() -> Call.Factory) = { { OkHttpClient() } }
+    var getIoDispatcher: () -> CoroutineContext = { Dispatchers.IO }
+    var getComputeDispatcher: () -> CoroutineContext = { Dispatchers.Default }
+    var getEventCoordinator: () -> EventCoordinator = { EventCoordinator(emptyList()) }
+    var getClock: () -> OidcClock = { OidcClock { Instant.now().epochSecond } }
+    var getIdTokenValidator: () -> IdTokenValidator = { DefaultIdTokenValidator() }
+    var getAccessTokenValidator: () -> AccessTokenValidator = { DefaultAccessTokenValidator() }
+    var getDeviceSecretValidator: () -> DeviceSecretValidator = { DefaultDeviceSecretValidator() }
+    var getCacheFactory: () -> suspend () -> Cache = { { SharedPreferencesCache.getInstance() } }
+    var getTokenStorageFactory: () -> suspend () -> TokenStorage = { { RoomTokenStorage.getInstance() } }
+    var getTokenEncryptionHandler: () -> TokenEncryptionHandler = { DefaultTokenEncryptionHandler() }
+    var getCookieJar: () -> CookieJar = { CookieJar.NO_COOKIES }
+    var getLoginCancellationDebounceTime: () -> Duration = { 0.seconds }
+}
