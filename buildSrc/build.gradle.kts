930bd414a7033932476986978e4374fad5be3150
Fei Chen <fei.chen@okta.com>
2026-02-02 14:37:04 -0800

    fix kdoc

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -12,6 +12,6 @@ repositories {
 
 dependencies {
     //noinspection UseTomlInstead
-    implementation("com.diffplug.spotless:spotless-plugin-gradle:8.2.0")
+    implementation("com.diffplug.spotless:spotless-plugin-gradle:8.2.1")
     implementation("org.owasp:dependency-check-gradle:12.2.0")
 }
